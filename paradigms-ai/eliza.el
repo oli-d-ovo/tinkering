ff50d5a2b4cba6452c263b9eac66025817f47c3c
Oli Desborough <oli.desborough@ovoenergy.com>
2017-09-20 17:51:25 +0100

    Added paradigms of AI and .spacemacs

---
+++ paradigms-ai/eliza.el
@@ -0,0 +1,69 @@
+;; -*- lexical-binding: t -*-
+
+(defconst fail nil)
+
+(defconst no-bindings '((t . t)))
+
+(defun get-binding (var bindings)
+  (assoc var bindings))
+
+(defun binding-val (binding)
+  (cdr binding))
+
+(defun lookup (var bindings)
+  (binding-val (get-binding var bindings)))
+
+(defun extend-bindings (var val bindings)
+  (cons (cons var val)
+        (unless (eq bindings no-bindings)
+          bindings)))
+
+(defun simple-equal (x y)
+  (if (or (atom x) (atom y))
+      (eql x y)
+    (and (simple-equal (first x) (first y))
+         (simple-equal (rest x) (rest y)))))
+
+(defun pat-match (pattern input)
+  (pat-match* pattern input no-bindings))
+
+(defun pat-match* (pattern input bindings)
+  (cond ((eq bindings fail) fail)
+        ((variable-p pattern)
+         (match-variable pattern input bindings))
+        ((eql pattern input) bindings)
+        ((segment-pattern-p pattern)
+         (segment-match pattern input bindings 0))
+        ((and (consp pattern) (consp input))
+         (pat-match* (rest pattern) (rest input)
+                     (pat-match* (first pattern) (first input)
+                                 bindings)))
+        (t fail)))
+
+(defun variable-p (x)
+  (and (symbolp x) (char-equal (string-to-char (symbol-name x)) ?$)))
+
+(defun match-variable (var input bindings)
+  (let ((binding (get-binding var bindings)))
+    (cond ((not binding) (extend-bindings var input bindings))
+          ((equal input (binding-val binding)) bindings)
+          (t fail))))
+
+(defun segment-pattern-p (pattern)
+  (and (consp pattern)
+       (consp (first pattern))
+       (equal (first (first pattern)) '$*)))
+
+(defun segment-match (pattern input bindings start)
+  (let ((var (first pattern))
+        (pat (rest pattern)))
+    (if (null pat)
+        (match-variable var input bindings)
+      (let ((pos (position (first pat) input
+                           :start start :test #'equal)))
+        (if (null pos)
+            fail
+          (let ((b2 (pat-match* pat (subseq input pos) bindings)))
+            (if (eq b2 fail)
+                (segment-match pattern input bindings (+ pos 1))
+              (match-variable var (subseq input 0 pos) b2))))))))

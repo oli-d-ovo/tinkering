c30de1bbff1b129035e80235bfd90b304e0d4ce7
Oli Desborough <oli.desborough@ovoenergy.com>
2017-09-21 17:52:45 +0100

    Working version

--- paradigms-ai/eliza.el
+++ paradigms-ai/eliza.el
@@ -2,21 +2,46 @@
 
 (defvar *eliza-rules*
   '(((($* $x) hello ($* $y))
-     (What\'s crackin\' holmes\?))
+     (What's crackin' holmes?))
 
     ((($* $x) I want ($* $y))
-     (Why do you want $y , yo\?)
-     (Say you got $y \. Then what\?)
+     (Why do you want $y , yo?)
+     (Say you got $y \. Then what?)
      (What would it mean if you got $y \?))))
 
 (defun eliza ()
   (loop
-   (message (present-response (read-from-minibuffer "eliza> ")))))
+   (print (eliza* (read-from-minibuffer "eliza> ")))))
+
+(defun eliza* (input)
+  (present-response
+   (use-eliza-rules (interpret-input input))))
+
+(defun use-eliza-rules (input)
+  (some #'(lambda (rule)
+            (let ((result (pat-match (rule-pattern rule) input)))
+              (when (not (eq result fail))
+                (sublis (switch-viewpoint result)
+                        (seq-random-elt (rule-responses rule))))))
+        *eliza-rules*))
+
+(defun switch-viewpoint (words)
+  (sublis '((I . you) (you . I) (me . you) (am . are))
+          words))
+
+(defun interpret-input (input)
+  (mapcar #'intern (split-string input)))
 
 (defun present-response (resp)
   (mapconcat 'identity
-             (mapcar #'symbol-name (apply #'append resp))
+             (mapcar #'symbol-name (flatten resp))
              " "))
+
+(defun flatten (mylist)
+  (cond
+   ((null mylist) nil)
+   ((atom mylist) (list mylist))
+   (t (append (flatten (car mylist)) (flatten (cdr mylist))))))
 
 (defconst fail nil)
 
